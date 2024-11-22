c93d8247ab9b2408aaf552d7204097e2280d0aee
dudurb17 <eduardo.robettibedin@gmail.com>
2024-11-22 14:31:28 -0300

    types of router

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,13 +1,19 @@
 import { View, Text } from "react-native";
 import React from "react";
-import { Slot } from "expo-router";
+import { Slot, Stack } from "expo-router";
 
 export default function Layout() {
   return (
-    <View style={{ flex: 1 }}>
-      <View style={{ width: "100%", height: 40, backgroundColor: "black" }} />
-      <Slot />
-      <View style={{ width: "100%", height: 40, backgroundColor: "red" }} />
-    </View>
+    // <View style={{ flex: 1 }}>
+    //   <View style={{ width: "100%", height: 40, backgroundColor: "black" }} />
+      // <Slot />
+      /* <View style={{ width: "100%", height: 40, backgroundColor: "red" }} />
+    </View> */
+    // <Stack>
+    //   <Stack.Screen name="index" options={{title:'Entrar'}}/>
+    //   <Stack.Screen name="sign-up" options={{title:'Criar Conta'}}/>
+
+    // </Stack>
+    <Stack screenOptions={{headerShown:false}}/>
   );
 }
