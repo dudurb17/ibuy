835c72edc45a894c763424c64d66d56e8e776150
dudurb17 <eduardo.robettibedin@gmail.com>
2024-11-22 13:59:56 -0300

    used navigate

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,26 +1,32 @@
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import React from "react";
-import { Link } from "expo-router";
+import { router } from "expo-router";
 
 export default function Index() {
+  function signUp() {
+    router.navigate('/sign-up')
+  }
   return (
     <View style={styles.container}>
-      <Link href="/sign-up" asChild>
+      {/* <Link href="/sign-up" asChild>
         <TouchableOpacity style={styles.button}>
           <Text style={styles.label}>Criar conta</Text>
         </TouchableOpacity>
-      </Link>
+      </Link> */}
+      <TouchableOpacity style={styles.button} onPress={signUp}>
+        <Text style={styles.label}>Criar conta</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: { flex: 1, justifyContent: "center", alignItems: "center" },
-  label: { fontSize: 16, fontWeight: "bold", color:'#FFF' },
-  button:{
-    backgroundColor:'black',
-    paddingHorizontal:32,
-    paddingVertical:10,
-    borderRadius:10
-  }
+  label: { fontSize: 16, fontWeight: "bold", color: "#FFF" },
+  button: {
+    backgroundColor: "black",
+    paddingHorizontal: 32,
+    paddingVertical: 10,
+    borderRadius: 10,
+  },
 });
